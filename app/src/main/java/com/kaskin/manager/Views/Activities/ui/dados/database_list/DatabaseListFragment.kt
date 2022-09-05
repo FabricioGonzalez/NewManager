2a868725a6fe1299330b4928dd985706e165067e
Fabricio Gonzalez França Wach <fabriciogonzalezfr@gmail.com>
2022-09-05 06:35:43 -0300

    Inicio do processo de criação de interface para controle de banco de dados

--- app/src/main/java/com/kaskin/manager/Views/Activities/ui/dados/database_list/DatabaseListFragment.kt
+++ app/src/main/java/com/kaskin/manager/Views/Activities/ui/dados/database_list/DatabaseListFragment.kt
@@ -1,12 +1,18 @@
 package com.kaskin.manager.Views.Activities.ui.dados.database_list
 
+import android.content.ContentResolver
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.kaskin.manager.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kaskin.manager.Views.Adapters.ExternalFileDbAdapter
+import com.kaskin.manager.databinding.FragmentDatabaseListBinding
 
 class DatabaseListFragment : Fragment() {
 
@@ -14,19 +20,120 @@ class DatabaseListFragment : Fragment() {
         fun newInstance() = DatabaseListFragment()
     }
 
+    private lateinit var externalFileDbAdapter: ExternalFileDbAdapter
+
+    private lateinit var contentResolver: ContentResolver
+
     private lateinit var viewModel: DatabaseListViewModel
+
+    private var _binding: FragmentDatabaseListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_database_list, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[DatabaseListViewModel::class.java]
+
+        _binding = FragmentDatabaseListBinding.inflate(inflater, container, false)
+
+        externalFileDbAdapter = ExternalFileDbAdapter()
+
+        contentResolver = requireContext().contentResolver
+
+        checkPermission(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            PackageManager.PERMISSION_GRANTED
+        )
+        checkPermission(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            PackageManager.PERMISSION_GRANTED
+        )
+
+        setupExternalStorageFiles()
+
+        loadFiles()
+        setupExternalStorageFiles()
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DatabaseListViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun loadFiles() {
+
+        val files = GetFilesFromExternalStorage()
+        externalFileDbAdapter.SetDataSet(files.toTypedArray())
+
+
     }
+
+    private fun setupExternalStorageFiles() = binding.dbList?.apply {
+        adapter = externalFileDbAdapter
+        layoutManager =
+            LinearLayoutManager(this.context)
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (context?.applicationContext!!.checkPermission(
+                permission, requestCode, 0
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                emptyArray(),
+                requestCode
+            );
+        } else {
+            Toast.makeText(requireActivity(), "Permission already granted", Toast.LENGTH_SHORT)
+                .show();
+        }
+    }
+
+    private fun GetFilesFromExternalStorage(): List<String> {
+        var items: List<String> = emptyList()
+/*
+        val collection = MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+
+
+        val projection = arrayOf(
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DISPLAY_NAME,
+        )
+
+        contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            "${MediaStore.Files.FileColumns.DISPLAY_NAME} ASC"
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndex(MediaStore.Files.FileColumns._ID)
+            val nameColumn = cursor.getColumnIndex(MediaStore.Files.FileColumns.DISPLAY_NAME)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val displayName = cursor.getString(nameColumn)
+
+                println(id)
+                items.plus(displayName)
+            }
+        }
+*/
+
+        val directory = this.context?.applicationContext?.getExternalFilesDir("/Manager")
+        val result = directory?.listFiles()
+//
+//        var file = File("${directory?.absolutePath}/manager.db")
+//        file.createNewFile()
+//        file.writeText("")
+
+        result?.forEach {
+            items = items.plus(it?.absolutePath ?: "not found")
+        }
+        return items
+
+    }
+
 
 }
